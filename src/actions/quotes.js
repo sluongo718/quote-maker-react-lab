993d3954f8bb1f27efcfdf2662cb14e7ca8564e6
sluongo718 <sluongo718@yahoo.com>
2021-09-17 23:18:49 -0400

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,5 +1,6 @@
 // TODO: Create action creators as defined in tests
 export const addQuote = (quote) => {
+ 
     return{
         type: "ADD_QUOTE",
         quote
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -3,14 +3,34 @@ export default (state = [], action) => {
     switch (action.type) {
 
       case "ADD_QUOTE": 
+      
         const newQuote = {
           id: action.quote.id,
           content: action.quote.content,
           author: action.quote.author,
-          votes: quote.action.vote
+          votes: action.quote.votes
         }
 
-        return (state = [...state, newQuote] );
+        return ( state = [...state, newQuote] );
+
+        case "REMOVE_QUOTE": 
+
+            return( state = state.filter( (quote) => quote.id !== action.quoteId ))
+
+        case "UPVOTE_QUOTE":
+
+            return( state = state.map((quote) => {
+              quote.id === action.quoteId && (quote.votes += 1);
+              return quote 
+            }) )
+
+          case "DOWNVOTE_QUOTE": 
+
+            return( state = state.map((quote) => {
+              quote.id === action.quoteId && quote.votes !== 0 && (quote.votes -= 1)
+
+              return quote
+            }) )
 
         default: 
                 return state
